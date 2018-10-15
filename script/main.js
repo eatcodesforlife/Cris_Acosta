5bf583c9d5c48fdaf0da481df217d90f07f6f897
Cris Acosta <iamcris87@gmail.com>
2018-10-15 17:46:30 +0800

    set bg to false: image taking too long to load, edited about me

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-// selec DOM items
+// select DOM items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
